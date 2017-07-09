48365c4c7a612483eb5a456aea03455bcc6dce01
Spaicy <spaicygraphic@gmail.com>
2017-07-09 20:51:16 +0200

    Update PotionStackerListener.java

--- AutoPotionStacker/src/me/spaicygaming/autopotionstacker/PotionStackerListener.java
+++ AutoPotionStacker/src/me/spaicygaming/autopotionstacker/PotionStackerListener.java
@@ -1,11 +1,15 @@
 package me.spaicygaming.autopotionstacker;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PotionStackerListener implements Listener{
@@ -32,6 +36,40 @@ public class PotionStackerListener implements Listener{
 					p.setItemInHand(item);
 					p.updateInventory();
 				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e){
+		if (e.getWhoClicked() instanceof Player && e.getInventory().equals(InventoryType.BREWING) && e.getRawSlot() < 4){
+			Player p = (Player)e.getWhoClicked();
+			ItemStack item = p.getItemOnCursor();
+			
+			if (item != null && !main.getConfig().getBoolean("AutoPotionStacker.brewStackedPotions") && item.getType().equals(Material.POTION)){
+				if (item.getAmount() > 1){
+					e.setCancelled(true);
+					e.getInventory().clear(e.getRawSlot());
+					item.setAmount(item.getAmount());
+					p.setItemOnCursor(item);
+					p.updateInventory();
+				}
+			}
+		}
+	}
+	
+	//Notify Updates
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		if (p.isOp() || p.hasPermission("autopotionstacker.notify") || p.hasPermission("*")){
+			if(main.updates.length == 2){
+				p.sendMessage(ChatColor.GREEN + main.Separatori(31));
+				p.sendMessage("§6§l[§cConsoleOnly§6] New update available:");
+				p.sendMessage("§6Current version: §e" + main.getDescription().getVersion());
+				p.sendMessage("§6New version: §e" + main.updates[0]);
+				p.sendMessage("§6What's new: §e" + main.updates[1]);
+				p.sendMessage(ChatColor.GREEN + main.Separatori(31));
 			}
 		}
 	}
